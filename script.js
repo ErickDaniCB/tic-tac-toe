09d39661745d0c194d4de7c557891b79b6e25f22
ErickDaniCB <erickdani10@hotmail.com>
2023-09-25 18:19:51 -0600

    Add draw and restart logic

--- script.js
+++ script.js
@@ -8,29 +8,25 @@ const PlayerFactory = (name, marker) => {
 const player1 = PlayerFactory("Pobi", "X");
 const player2 = PlayerFactory("Polo", "O");
 
-//BOARD MODULE ///
+//BOARD
 
-const gameBoard = (() => {
-  const createBoard = () => {
-    let grid = [];
-    for (let i = 0; i < 3; i++) {
-      const arr = [];
-      for (let j = 0; j < 3; j++) {
-        arr.push("");
-      }
-      grid.push(arr);
+const createBoard = () => {
+  let grid = [];
+  for (let i = 0; i < 3; i++) {
+    const arr = [];
+    for (let j = 0; j < 3; j++) {
+      arr.push("");
     }
-    return grid;
-  };
+    grid.push(arr);
+  }
+  return grid;
+};
 
-  let board = createBoard();
-
-  return { board };
-})();
 
 // GAME FLOW MODULE ///
 
 const gameFlow = (() => {
+  let board = createBoard();
   let currentPlayer = player1;
 
   const switchPlayer = () => {
@@ -42,10 +38,20 @@ const gameFlow = (() => {
   };
 
   const makePlay = (cellRow, cellColumn, player) => {
-    if (gameBoard.board[cellRow][cellColumn] === "") {
+    if (board[cellRow][cellColumn] === "") {
       player = currentPlayer;
-      gameBoard.board[cellRow][cellColumn] = player.marker;
-      checkWinner();
+      board[cellRow][cellColumn] = player.marker;
+      console.table(board);
+
+      const checkWin = checkEach();
+      const checkFilled = isFilled();
+      if (checkWin) {
+        console.log(`${currentPlayer.marker} wins!`);
+        restart();
+      } else if (checkFilled) {
+        console.log("Draw!");
+        restart();
+      }
       currentPlayer = switchPlayer();
     }
   };
@@ -55,7 +61,7 @@ const gameFlow = (() => {
   //BY ROWS
   const rowArr = () => {
     const arrString = [];
-    gameBoard.board.forEach((row) => {
+    board.forEach((row) => {
       let joined = row.join("");
       arrString.push(joined);
     });
@@ -65,7 +71,7 @@ const gameFlow = (() => {
   //BY COLUMNS
   const columnArr = () => {
     const columnString = ["", "", ""];
-    gameBoard.board.forEach((row) => {
+    board.forEach((row) => {
       for (let i = 0; i < 3; i++) {
         columnString[i] += row[i];
       }
@@ -79,16 +85,16 @@ const gameFlow = (() => {
     for (let i = 0; i < 3; i++) {
       switch (i) {
         case 0:
-          diagonalString[0] += gameBoard.board[i][0];
-          diagonalString[1] += gameBoard.board[i][2];
+          diagonalString[0] += board[i][0];
+          diagonalString[1] += board[i][2];
           break;
         case 1:
-          diagonalString[0] += gameBoard.board[i][1];
-          diagonalString[1] += gameBoard.board[i][1];
+          diagonalString[0] += board[i][1];
+          diagonalString[1] += board[i][1];
           break;
         case 2:
-          diagonalString[0] += gameBoard.board[i][2];
-          diagonalString[1] += gameBoard.board[i][0];
+          diagonalString[0] += board[i][2];
+          diagonalString[1] += board[i][0];
           break;
       }
     }
@@ -96,8 +102,8 @@ const gameFlow = (() => {
   };
 
   const checker = (arrMarkers) => {
-    let marker = `${currentPlayer.marker}`;
-    let pattern = `${marker}${marker}${marker}`;
+    const marker = `${currentPlayer.marker}`;
+    const pattern = `${marker}${marker}${marker}`;
     let win = false;
     arrMarkers.forEach((string) => {
       if (string === pattern) {
@@ -107,8 +113,7 @@ const gameFlow = (() => {
     return win;
   };
 
-  const checkWinner = () => {
-    console.log(currentPlayer.marker);
+  const checkEach = () => {
     const rowStrings = rowArr();
     const columnStrings = columnArr();
     const diagonalStrings = diagonalArr();
@@ -118,12 +123,28 @@ const gameFlow = (() => {
     const diagonalCheck = checker(diagonalStrings);
 
     if (rowCheck || columnCheck || diagonalCheck) {
-      console.log("End!");
-      display.cells.forEach((item) => {
-        item.removeEventListener("click", makePlay);
-      });
-    }
+      return true;
+    } else return false;
   };
+
+  //DRAW CONDITION:
+
+  const isFilled = () => {
+    let arrTemp = [];
+    board.forEach((arr) => {
+      arrTemp = arrTemp.concat(arr);
+    });
+    if(arrTemp.includes('')){
+      return false;
+    } return true;
+  };
+  
+  //RESTART GAME 
+  
+  const restart = () => { 
+    board = createBoard();
+    console.log(board);
+  }
 
   return { makePlay, getCurrentMarker };
 })();
@@ -131,18 +152,19 @@ const gameFlow = (() => {
 // DISPLAY GAME MODULE ///
 
 const display = (() => {
+
   const cells = document.querySelectorAll(".cell");
+
   cells.forEach((item) => {
+    const play = () => {
     const id = item.getAttribute("id");
-    const makePlay = () => {
       if (item.textContent === "") {
         item.textContent = gameFlow.getCurrentMarker();
         gameFlow.makePlay(id[0], id[1]);
       }
-    };
-    
-    item.addEventListener("click", makePlay);
+    }
+    item.addEventListener("click", play);
   });
 
-  return { cells };
+  return {} ;
 })();
